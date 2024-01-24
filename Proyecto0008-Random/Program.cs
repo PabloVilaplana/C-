a8de063125ed9d3d360a7975374e43a2b849c7bd
PabloVilaplana <40396181+PabloVilaplana@users.noreply.github.com>
2024-01-24 16:10:11 -0300

    Add files via upload

---
+++ Proyecto0008-Random/Program.cs
@@ -0,0 +1,9 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Random dice = new Random();
+        int roll = dice.Next(1, 7);
+        Console.WriteLine(roll);
+    }
+}
