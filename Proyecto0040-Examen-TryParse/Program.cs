a471a65003233bf1c13c88efb03db36a662c175d
PabloVilaplana <40396181+PabloVilaplana@users.noreply.github.com>
2024-01-24 16:27:03 -0300

    Add files via upload

---
+++ Proyecto0040-Examen-TryParse/Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        string value = "bad";
+        int result = 0;
+        if (int.TryParse(value, out result))
+        {
+            Console.WriteLine($"Measurement: {result}");
+        }
+        else
+        {
+            Console.WriteLine("Unable to report the measurement.");
+        }
+
+        if (result > 0)
+        {
+            Console.WriteLine($"Measurement (w/ offset): {50 + result}");
+        }
+
+
+
+
+    }
+}
