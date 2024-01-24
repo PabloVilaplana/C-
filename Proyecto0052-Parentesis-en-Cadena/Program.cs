c0fa5a9f4bb39546c5cbd4d2b340fe65797f6289
PabloVilaplana <40396181+PabloVilaplana@users.noreply.github.com>
2024-01-24 16:33:44 -0300

    Add files via upload

---
+++ Proyecto0052-Parentesis-en-Cadena/Program.cs
@@ -0,0 +1,19 @@
+﻿
+string message = "Find what is (inside the parentheses)";
+
+int openingPosition = message.IndexOf('(');
+int closingPosition = message.IndexOf(')');
+
+openingPosition += 1;
+
+int length = closingPosition - openingPosition;
+Console.WriteLine(message.Substring(openingPosition, length));
+
+string message2 = "What is the value <span>between the tags</span>?";
+
+int openingPosition2 = message2.IndexOf("<span>");
+int closingPosition2 = message2.IndexOf("</span>");
+
+openingPosition += 6;
+int length2 = closingPosition2 - openingPosition2;
+Console.WriteLine(message2.Substring(openingPosition2, length2));
