685d5d0cbe3e5f915a2fc85d742d3011a16bc328
PabloVilaplana <40396181+PabloVilaplana@users.noreply.github.com>
2024-01-24 16:23:02 -0300

    Add files via upload

---
+++ Proyecto0030-DoWhile/Program.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Random random = new Random();
+        int ataqueHeroe = random.Next(1, 11);
+        int ataqueMonstruo = random.Next(1, 11);
+
+        int vidaHeroe = 10;
+        int vidaMonstruo = 10;
+
+        do
+        {
+            vidaMonstruo = vidaMonstruo - ataqueHeroe;
+            Console.WriteLine($"Monster was damaged and lost {ataqueHeroe} health and now has {vidaMonstruo} health.");
+            if ( vidaMonstruo <= 0 )
+            {
+                Console.WriteLine($"Ganó el Heroe");
+                break;
+            }
+
+            vidaHeroe = vidaHeroe - ataqueMonstruo;
+            Console.WriteLine($"Hero was damaged and lost {ataqueMonstruo} health and now has {vidaHeroe} health.");
+            if (vidaHeroe <= 0)
+            {
+                Console.WriteLine($"Ganó el Monstruo");
+                break;
+            }
+
+        } while (vidaMonstruo > 0 || vidaHeroe > 0 );
+    }
+}
---
+++ Proyecto0031-DoWhile/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string? readResult;
+        Console.WriteLine("Enter a string:");
+        do
+        {
+            readResult = Console.ReadLine();
+        } while (readResult == null);
+
+        // capture user input in a string variable named readResult
+
+        int numericValue = 0;
+        bool validNumber = false;
+
+        validNumber = int.TryParse(readResult, out numericValue);
+    }
+}
