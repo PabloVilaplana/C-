10a8c4551564a91080de1a9e5e9b64f074a7a422
PabloVilaplana <40396181+PabloVilaplana@users.noreply.github.com>
2024-01-24 16:31:09 -0300

    Add files via upload

---
+++ Proyecto0048-Formato-Compuesto/Program.cs
@@ -0,0 +1,26 @@
+﻿
+string first = "Hello";
+string second = "World";
+
+/*
+Console.WriteLine("{1} {0}!", first, second);
+Console.WriteLine("{0} {0} {0}!", first, second);
+*/
+
+Console.WriteLine($"{first} {second}!");
+Console.WriteLine($"{second} {first}!");
+Console.WriteLine($"{first} {first} {first}!");
+
+decimal price = 123.45m;
+int discount = 50;
+Console.WriteLine($"Price: {price:C} (Save {discount:C})");
+
+decimal measurement = 123456.78912m;
+Console.WriteLine($"Measurement: {measurement:N} units");
+
+decimal measurement2 = 123456.78912m;
+Console.WriteLine($"Measurement: {measurement2:N4} units");
+
+decimal tax = .36785m;
+Console.WriteLine($"Tax rate: {tax:P2}");
+
