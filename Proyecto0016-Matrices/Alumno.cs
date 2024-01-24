cdf93925322872d4a819a4499e4138df014bab89
PabloVilaplana <40396181+PabloVilaplana@users.noreply.github.com>
2024-01-24 16:11:41 -0300

    Add files via upload

---
+++ Proyecto0016-Matrices/Alumno.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colegio
+{
+    internal class Alumno
+    {
+
+        public Alumno(String nombre, int nota1, int nota2, int nota3, int nota4, int nota5)
+        {
+            alumnoNombre = nombre;
+            alumnoNota1 = nota1;
+            alumnoNota2 = nota2;
+            alumnoNota3 = nota3;
+            alumnoNota4 = nota4;
+            alumnoNota5 = nota5;
+        }
+
+        private String alumnoNombre;
+        private int alumnoNota1;
+        private int alumnoNota2;
+        private int alumnoNota3;
+        private int alumnoNota4;
+        private int alumnoNota5;
+
+        public void getNotas()
+        {
+            Console.WriteLine($"la Nota 1 es: {alumnoNota1}");
+            Console.WriteLine($"la Nota 2 es: {alumnoNota2}");
+            Console.WriteLine($"la Nota 3 es: {alumnoNota3}");
+            Console.WriteLine($"la Nota 4 es: {alumnoNota4}");
+            Console.WriteLine($"la Nota 5 es: {alumnoNota5}");
+        }
+
+        public void getGlobal()
+        {
+            decimal alumnoGlobal = (alumnoNota1 + alumnoNota2 + alumnoNota3 + alumnoNota4 + alumnoNota5) / 5;
+            Console.WriteLine($"la Nota Global es: {alumnoGlobal}");
+        }
+
+        public void getNombre()
+        {
+            Console.WriteLine($"El nombre del Alumno es: {alumnoNombre}");
+        }
+
+    }
+}
---
+++ Proyecto0016-Matrices/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Colegio;
+
+namespace Colegio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random random = new Random();
+
+            string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan" };
+
+            int nota1 = random.Next(1, 100);
+            int nota2 = random.Next(1, 100);
+            int nota3 = random.Next(1, 100);
+            int nota4 = random.Next(1, 100);
+            int nota5 = random.Next(1, 100);
+            Alumno Alumno1 = new Alumno(studentNames[0], nota1, nota2, nota3, nota4, nota5);
+            Alumno1.getNombre();
+            Alumno1.getNotas();
+            Alumno1.getGlobal();
+            Console.WriteLine("\n");
+
+
+            nota1 = random.Next(1, 100);
+            nota2 = random.Next(1, 100);
+            nota3 = random.Next(1, 100);
+            nota4 = random.Next(1, 100);
+            nota5 = random.Next(1, 100);
+            Alumno Alumno2 = new Alumno(studentNames[1], nota1, nota2, nota3, nota4, nota5);
+            Alumno2.getNombre();
+            Alumno2.getNotas();
+            Alumno2.getGlobal();
+            Console.WriteLine("\n");
+
+
+            nota1 = random.Next(1, 100);
+            nota2 = random.Next(1, 100);
+            nota3 = random.Next(1, 100);
+            nota4 = random.Next(1, 100);
+            nota5 = random.Next(1, 100);
+            Alumno Alumno3 = new Alumno(studentNames[2], nota1, nota2, nota3, nota4, nota5);
+            Alumno3.getNombre();
+            Alumno3.getNotas();
+            Alumno3.getGlobal();
+            Console.WriteLine("\n");
+
+
+            nota1 = random.Next(1, 100);
+            nota2 = random.Next(1, 100);
+            nota3 = random.Next(1, 100);
+            nota4 = random.Next(1, 100);
+            nota5 = random.Next(1, 100);
+            Alumno Alumno4 = new Alumno(studentNames[3], nota1, nota2, nota3, nota4, nota5);
+            Alumno4.getNombre();
+            Alumno4.getNotas();
+            Alumno4.getGlobal();
+            Console.WriteLine("\n");
+
+        }
+    }
+
+}
