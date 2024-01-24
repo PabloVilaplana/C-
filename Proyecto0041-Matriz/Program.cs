ba1a92c2219759484e4853ade468a8b1377c935b
PabloVilaplana <40396181+PabloVilaplana@users.noreply.github.com>
2024-01-24 16:28:28 -0300

    Add files via upload

---
+++ Proyecto0041-Matriz/Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] values = { "12,3", "45", "ABC", "11", "DEF" };
+        string valor = "";
+        double valornumerico = 0;
+
+        string mensaje = "";
+        double total = 0;
+
+        for (int i = 0; i < values.Length; i++)
+        {
+            valor = values[i];
+
+            if (double.TryParse(valor, out valornumerico))
+            {
+                total = total + valornumerico;
+            }
+            else
+            {
+                mensaje += valor;
+            }
+        }
+
+        Console.WriteLine($"Message: {mensaje}");
+        Console.WriteLine($"Total: {total}");
+    }
+}
