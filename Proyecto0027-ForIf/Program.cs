f5076b556f8ede17671f881a5941ef07ce4ed0fc
PabloVilaplana <40396181+PabloVilaplana@users.noreply.github.com>
2024-01-24 16:21:38 -0300

    Add files via upload

---
+++ Proyecto0027-ForIf/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string nombre = "";
+        for (int i = 1; i <= 100; i++)
+        {
+            nombre = "";
+            if (i % 3 == 0) nombre = "Fizz";
+            if (i % 5 == 0) nombre = "Buzz";
+            if (i % 3 == 0 && i % 5 == 0) nombre = "FizzBuzz";
+
+
+            Console.WriteLine($"{i} - {nombre}");
+        }
+    }
+}
---
+++ Proyecto0028-DoWhile/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Random random = new Random();
+        int current = 0;
+
+        do
+        {
+            current = random.Next(1, 11);
+            Console.WriteLine(current);
+        } while (current != 7);
+    }
+}
