bd99612c486b004ffd8243461c2413a1bb51d39e
PabloVilaplana <40396181+PabloVilaplana@users.noreply.github.com>
2024-01-24 16:24:41 -0300

    Add files via upload

---
+++ Proyecto0032-DoWhile/Program.cs
@@ -0,0 +1,39 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string? readResult;
+        Console.WriteLine("Ingrese un numero del 5 al 10");
+        do
+        {
+            readResult = Console.ReadLine();
+        } while (readResult == null);
+
+        // capture user input in a string variable named readResult
+
+        int numericValue = 0;
+        bool validNumber = false;
+
+        validNumber = int.TryParse(readResult, out numericValue);
+
+        if (validNumber == false || numericValue <= 4 || numericValue >= 11)
+        {
+            do
+            {
+                Console.WriteLine("Ingresaste un Caracter no valido, ingresa otra vez");
+                do
+                {
+                    readResult = Console.ReadLine();
+                } while (readResult == null);
+
+                numericValue = 0;
+                validNumber = false;
+
+                validNumber = int.TryParse(readResult, out numericValue);
+            } while (validNumber == false || numericValue <= 4 || numericValue >= 11);
+        }
+
+        Console.WriteLine($"Su numero es {numericValue}");
+
+    }
+}
---
+++ Proyecto0033-DoWhile/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+        string? readResult;
+        Console.WriteLine("Ingrese un rol: Administrador, Director o Usuario");
+        do
+        {
+            readResult = Console.ReadLine();
+            readResult = readResult.Trim().ToLower();
+            if (readResult != "administrador" && readResult != "director" && readResult != "usuario")
+            {
+                Console.WriteLine("Ingrese un rol valido");
+            }
+        } while (readResult != "administrador" && readResult != "director" && readResult != "usuario");
+
+        
+        Console.WriteLine($"Su Rol es {readResult}");
+    }
+}
